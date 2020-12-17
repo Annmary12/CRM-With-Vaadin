e6a9115d0763dc17d372513a12cf1fa4975317bf
Annmary <annmaryamaka@yahoo.com>
2020-12-17 08:53:06 +0100

    add filter

--- src/main/java/com/example/ui/MainView.java
+++ src/main/java/com/example/ui/MainView.java
@@ -5,6 +5,8 @@ import com.example.backend.entity.Contact;
 import com.example.backend.service.ContactService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,19 +14,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class MainView extends VerticalLayout {
   private ContactService contactService;
   private Grid<Contact> grid = new Grid<>(Contact.class);
+  private TextField filterText = new TextField();
 
   public MainView(ContactService contactService) {
     this.contactService = contactService;
     addClassName("list-view");
     setSizeFull();
+    configureFilter();
     configureGrid();
 
-    add(grid);
+    add(filterText, grid);
     updateList();
     }
 
     private void configureGrid(){
       grid.addClassName("contact-grid");
+//      grid.setWidth("20");
       grid.setSizeFull();
       grid.setColumns("firstName", "lastName", "email", "status");
       grid.addColumn(contact -> {
@@ -34,7 +39,14 @@ public class MainView extends VerticalLayout {
       grid.getColumns().forEach(col -> col.setAutoWidth(true));
     }
 
+    private void configureFilter(){
+      filterText.setPlaceholder("Filter by name...");
+      filterText.setClearButtonVisible(true);
+      filterText.setValueChangeMode(ValueChangeMode.LAZY);
+      filterText.addValueChangeListener(e -> updateList());
+    }
+
     private void updateList(){
-      grid.setItems(contactService.findAll());
+      grid.setItems(contactService.findAll(filterText.getValue()));
     }
 }
